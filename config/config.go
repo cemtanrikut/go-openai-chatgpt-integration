dabb0b0ae5a6bd5fd63a27f4661b8b24851539eb
Cem Tanrikut <cemtanrikut@gmail.com>
2025-01-27 03:35:15 +0100

    add godotenv file for read .env files

--- config/config.go
+++ config/config.go
@@ -1,1 +1,20 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found. Using system environment variables.")
+	}
+
+	// if .env file doesnt exist check OS getenv
+	if os.Getenv("OPENAI_API_KEY") == "" {
+		log.Fatal("OPENAI_API_KEY is not set")
+	}
+}
