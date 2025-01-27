96f7c4e80094f83706d596afb757f3ee82a59c94
Cem Tanrikut <cemtanrikut@gmail.com>
2025-01-27 04:10:32 +0100

    add router in main file

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/cemtanrikut/go-openai-chatgpt-integration/config"
+import (
+	"log"
+	"net/http"
+
+	"github.com/cemtanrikut/go-openai-chatgpt-integration/config"
+	"github.com/cemtanrikut/go-openai-chatgpt-integration/handler"
+)
 
 func main() {
 	// Load environment variables
 	config.LoadEnv()
+
+	// Define routes
+	http.HandleFunc("/chat", handler.ChatHandler)
+
+	// Start the server
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
